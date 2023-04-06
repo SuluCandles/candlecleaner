ae599ce539d2dcdee702fea15b52a046be46daa3
BuildTools <me@filip.tech>
2023-04-06 15:25:44 -0400

    updated build file

--- src/build.py
+++ src/build.py
@@ -1,12 +1,20 @@
 import subprocess
 import os
+import argparse
+
+# Parse the command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--platform", help="the platform to build for", choices=["windows", "macos"], required=True)
+args = parser.parse_args()
 
 # Get the directory where this script is located
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 # Set the paths to the icon files
-#mac_icon_path = os.path.join(script_dir, 'resources', 'icon.icns')
-win_icon_path = os.path.join(script_dir, 'icon.ico')
+if args.platform == "windows":
+    icon_path = os.path.join(script_dir, 'icon.ico')
+else:
+    icon_path = os.path.join(script_dir, 'icon.icns')
 
 # Set the path to the main script
 main_script_path = os.path.join(script_dir, 'candlecleaner.py')
@@ -17,6 +25,9 @@ work_dir = os.path.dirname(script_dir).removesuffix("src")
 work_dir = os.path.join(work_dir, 'build/')
 
 # Build the application using PyInstaller
-command = f'pyinstaller --onefile --add-data "{win_icon_path};." --windowed --name candlecleaner --icon "{win_icon_path}" --distpath "{work_dir}" --workpath "{work_dir}" --specpath {work_dir} {main_script_path}'
+if args.platform == "windows":
+    command = f'pyinstaller --onefile --add-data "{icon_path};." --windowed --name candlecleaner --icon "{icon_path}" --distpath "{work_dir}" --workpath "{work_dir}" --specpath {work_dir} {main_script_path}'
+else:
+    command = f'pyinstaller --onefile --add-data "{icon_path}:." --windowed --name candlecleaner --icon "{icon_path}" --distpath "{work_dir}" --workpath "{work_dir}" --specpath {work_dir} {main_script_path}'
 print(command)
 subprocess.run(command, shell=True)
